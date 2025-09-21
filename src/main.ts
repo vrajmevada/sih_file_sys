513bf260d44e8aad2cfe15dbda7b957adee7037f
vrajmevada <vrajmevada1@gmail.com>
2025-09-21 22:07:57 +0530

    filesystem

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('KMRL Document Management API')
+    .setDescription('File upload and management')
+    .setVersion('1.0.0')
+    .addTag('files')
+    .build();
+  const doc = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, doc);
+
+  await app.listen(3000);
+  console.log('API running on http://localhost:3000');
+  console.log('Swagger docs on http://localhost:3000/docs');
 }
 bootstrap();
